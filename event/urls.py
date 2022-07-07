fbc3998d52f015543629e403f304d20f3513f69c
abrshtg <abrshtgam@gmail.com>
2022-07-07 17:19:16 +0300

    add media and static file

--- event/urls.py
+++ event/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('birth/', views.birth, name='birth'),
+    path('death/', views.death, name='death'),
+    path('marriage/', views.marriage, name='marriage'),
+    path('divorce/', views.divorce, name='divorce')
 ]
--- event/views.py
+++ event/views.py
@@ -3,4 +3,16 @@ from . models import Birth, Death, Divorce, Marriage
 # Create your views here.
 
 def home(request):
-    pass
+    return render(request, 'event/home.html')
+
+def birth(request):
+    return render(request, 'event/birth.html')
+
+def death(request):
+    return render(request, 'event/death.html')
+
+def marriage(request):
+    return render(request, 'event/marriage.html')
+
+def divorce(request):
+    return render(request, 'event/divorce.html')
