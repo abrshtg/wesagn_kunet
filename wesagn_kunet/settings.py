82acd19930ff9491f1bd59458b05f9db0dafc8be
abrshtg <abrshtgam@gmail.com>
2022-07-08 03:16:14 +0300

    disable admin dark theme

--- wesagn_kunet/settings.py
+++ wesagn_kunet/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_non_dark_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,4 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DISABLE_DARK_MODE = True
