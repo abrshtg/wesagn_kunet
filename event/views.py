cee7645cb68ad6be512dd45f8282dda6986183d4
abrshtg <abrshtgam@gmail.com>
2022-07-08 05:08:34 +0300

    add view page for birth

--- event/views.py
+++ event/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from . models import Birth, Death, Divorce, Marriage
 # Create your views here.
 
+
 def home(request):
     return render(request, 'event/home.html')
 
+
 def birth(request):
-    return render(request, 'event/birth.html')
+    queryset = Birth.objects.all()
+    return render(request, 'event/birth.html', {'births': queryset})
+
 
 def death(request):
     return render(request, 'event/death.html')
 
+
 def marriage(request):
     return render(request, 'event/marriage.html')
+
 
 def divorce(request):
     return render(request, 'event/divorce.html')
